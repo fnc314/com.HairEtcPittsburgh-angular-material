bc5352bbfd7abc6ae75e6703d846d42183a600d1
Franco N. Colaizzi <fnc314@gmail.com>
2015-05-09 22:00:40 -0400

    third commit...coming together

---
+++ src/js/HairEtcService.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var HairEtcService = angular.module('HairEtcService',)
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Entry point for application
+// Where everything comes together
+/*
+ *
+ * 
+ */
+
+var hairEtcAngularMaterial = angular.module('hairEtcAngularMaterial', [
+    'ui.router', // Router
+    '', // Angular module - Animate
+    '', // Angular module - Aria
+    '', // Angular module - Material
+    ]);
