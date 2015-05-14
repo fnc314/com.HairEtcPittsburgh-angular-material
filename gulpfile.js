bdd8b4f6cbf4594f856610b22ea68544723f532a
Franco N. Colaizzi <fnc314@gmail.com>
2015-05-14 19:51:31 -0400

    fifth commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch');
+
+var paths = {
+    scripts: ['src/js/*.js'],
+    
+}
 
 gulp.task('minify', function () {
     gulp.src('js/app.js')
